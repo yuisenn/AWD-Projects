c55bbf72ebcb18064a696b8b20ba2e10cd3ff273
yuisenn <157201702+yuisenn@users.noreply.github.com>
2024-01-20 16:50:47 +0800

    Add files via upload

---
+++ Notification Bell/JS/script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('_button');
+const notification = document.getElementById('_notification');
+
+button.addEventListener('click', () =>{
+    const counter = Number(notification.getAttribute('data-count'))
+
+    notification.setAttribute('data-count', counter +1);
+    notification.classList.add('count');
+    notification.classList.add('notify');
+    button. classList.toggle('active');
+})
+
+notofication.addEventListener('animationed', () =>{
+    notification.classList.remove('notify');
+    button.classList.remove('active');
+})
